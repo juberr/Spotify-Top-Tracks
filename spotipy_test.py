386cb33d190cf9da1543d72d3caca2f2ebca3947
justnberry <justn.berry@gmail.com>
2020-10-06 21:45:58 -0400

    Turned file into functions

--- spotipy_test.py
+++ spotipy_test.py
@@ -5,8 +5,16 @@ import requests
 from datetime import date
 import secrets
 
+scope = 'user-top-read playlist-modify-private'
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=secrets.client_id,
+                                               client_secret=secrets.client_secret,
+                                               scope=scope,
+                                               redirect_uri='http://127.0.0.1:9090'))
+user = sp.current_user()
 
-def make_monthly_playlist():
+
+# Get current month
+def get_month():
     todays_date = date.today()
     current_month = todays_date.month
     months_reference = {
@@ -24,24 +32,29 @@ def make_monthly_playlist():
         12: 'December'
     }
     current_month_name = months_reference[current_month]
+    return current_month_name
 
-    scope = 'user-top-read playlist-modify-private'
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=secrets.client_id,
-                                                   client_secret=secrets.client_secret,
-                                                   scope=scope,
-                                                   redirect_uri='http://127.0.0.1:9090'))
 
+# Get user's top tracks
+def get_top_tracks():
     results = sp.current_user_top_tracks(limit=50, time_range='medium_term')
-    user = sp.current_user()
+    return results
 
+
+# Creates a playlist based on what month it is
+def create_monthly_playlist():
+    current_month_name = get_month()
     ret = sp.user_playlist_create(user['id'], f'{current_month_name} 2020 Top Tunes', False, False,
                                   f'50 of your most listened to songs in {current_month_name}')
-
     real_playlist_name = ret['id']
+    return real_playlist_name
 
+
+# adds top tracks to created playlist, takes the playlist ID generated from create_monthly_playlist as an argument
+def add_songs_to_ps(playlistname):
     func_list = []
+    results = get_top_tracks()
     for i in results['items']:
         current_track = i['uri']
         func_list += [current_track]
-
-    sp.playlist_add_items(real_playlist_name, func_list)
+    sp.playlist_add_items(playlistname, func_list)
